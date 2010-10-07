5571ec16a829837b6ffe09ecbaa674d51ced199c
rafael.soto <rafael.soto@e6791326-9150-4ff1-db7a-2f0afe25d0be>
2010-10-07 22:28:20 +0000

    Incluindo arquitetura da aplicacao

---
+++ implementation/app/geodojo/src/main/java/org/latinoware/geodojo/hsqldb/DatabaseListener.java
@@ -0,0 +1,35 @@
+package org.latinoware.geodojo.hsqldb;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.hsqldb.Server;
+
+/**
+ * Listener de aplicacao para iniciar um servidor HSQLDB em memoria
+ * @author Rafael Soto
+ *
+ */
+public class DatabaseListener implements ServletContextListener {
+
+	private final Server server;
+
+	public DatabaseListener() {
+		server = new Server();
+		server.setDatabaseName(0, "geodojodb");
+		server.setDatabasePath(0, "db/geodojodb");
+		server.setPort(9001);
+		server.setSilent(false);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		server.stop();
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		server.start();
+	}
+
+}
