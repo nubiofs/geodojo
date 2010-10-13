e763d3853af6f30fab76efd82d513ca1d4051d10
rafael.soto <rafael.soto@e6791326-9150-4ff1-db7a-2f0afe25d0be>
2010-10-13 13:31:37 +0000

    Correção da classe entitymanager bean. Voltamos a versao do JPA para ficar compativel com o HBSpatial e algumas operações passaram a não existir

--- implementation/app/geodojo/src/main/java/org/latinoware/geodojo/beans/EntityManagerBean.java
+++ implementation/app/geodojo/src/main/java/org/latinoware/geodojo/beans/EntityManagerBean.java
@@ -1,7 +1,6 @@
 package org.latinoware.geodojo.beans;
 
 import java.io.Serializable;
-import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.SessionScoped;
@@ -12,10 +11,6 @@ import javax.persistence.FlushModeType;
 import javax.persistence.LockModeType;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.metamodel.Metamodel;
 
 /**
  * Managed Bean(CDI) criado para disponibilizar o EntityManager em toda a aplicacao.
@@ -26,246 +21,162 @@ import javax.persistence.metamodel.Metamodel;
  */
 @SuppressWarnings("serial")
 @SessionScoped
-public class EntityManagerBean implements EntityManager, Serializable {
-
-    private EntityManager emDelegate;
-
-    @PostConstruct
-    public void createEntityManager() {
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("geodojodbspatial");
-        emDelegate = emf.createEntityManager();
-
-    }
-
-    public void destroyEntityManager() {
-        if (emDelegate == null) {
-            emDelegate.flush();
-            emDelegate.close();
-        }
-    }
-
-    @Override
-    public void clear() {
-
-        this.emDelegate.clear();
-
-    }
-
-    @Override
-    public void close() {
-
-        this.emDelegate.close();
-
-    }
-
-    @Override
-    public boolean contains(Object arg0) {
-
-        return this.emDelegate.contains(arg0);
-    }
-
-    @Override
-    public Query createNamedQuery(String arg0) {
-
-        return this.emDelegate.createNamedQuery(arg0);
-    }
-
-    @Override
-    public Query createNativeQuery(String arg0) {
-
-        return this.emDelegate.createNamedQuery(arg0);
-    }
-
-    @Override
-    public Query createNativeQuery(String arg0, Class arg1) {
-
-        return this.emDelegate.createNativeQuery(arg0, arg1);
-    }
-
-    @Override
-    public Query createNativeQuery(String arg0, String arg1) {
-
-        return this.emDelegate.createNativeQuery(arg0, arg1);
-    }
-
-    @Override
-    public Query createQuery(String arg0) {
-
-        return this.emDelegate.createQuery(arg0);
-    }
-
-    @Override
-    public <T> T find(Class<T> arg0, Object arg1) {
-
-        return this.emDelegate.find(arg0, arg1);
-    }
-
-    @Override
-    public void flush() {
-        this.emDelegate.flush();
-
-    }
-
-    @Override
-    public Object getDelegate() {
-
-        return this.emDelegate.getDelegate();
-    }
-
-    @Override
-    public FlushModeType getFlushMode() {
-        // TODO Auto-generated method stub
-        return this.emDelegate.getFlushMode();
-    }
-
-    @Override
-    public <T> T getReference(Class<T> arg0, Object arg1) {
-        // TODO Auto-generated method stub
-        return this.emDelegate.getReference(arg0, arg1);
-    }
-
-    @Override
-    public EntityTransaction getTransaction() {
-        // TODO Auto-generated method stub
-        return this.emDelegate.getTransaction();
-    }
-
-    @Override
-    public boolean isOpen() {
-        // TODO Auto-generated method stub
-        return this.emDelegate.isOpen();
-    }
-
-    @Override
-    public void joinTransaction() {
-        this.emDelegate.joinTransaction();
-
-    }
-
-    @Override
-    public void lock(Object arg0, LockModeType arg1) {
-        this.emDelegate.lock(arg0, arg1);
-
-    }
-
-    @Override
-    public <T> T merge(T arg0) {
-
-        return this.emDelegate.merge(arg0);
-    }
-
-    @Override
-    public void persist(Object arg0) {
-        this.emDelegate.persist(arg0);
-
-    }
-
-    @Override
-    public void refresh(Object arg0) {
-        this.emDelegate.refresh(arg0);
-
-    }
-
-    @Override
-    public void remove(Object arg0) {
-        this.emDelegate.remove(arg0);
-
-    }
-
-    @Override
-    public void setFlushMode(FlushModeType arg0) {
-        this.emDelegate.setFlushMode(arg0);
-
-    }
-
-    @Override
-    public <T> T find(Class<T> arg0, Object arg1, Map<String, Object> arg2) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public <T> T find(Class<T> arg0, Object arg1, LockModeType arg2) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public <T> T find(Class<T> arg0, Object arg1, LockModeType arg2, Map<String, Object> arg3) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void lock(Object arg0, LockModeType arg1, Map<String, Object> arg2) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void refresh(Object arg0, Map<String, Object> arg1) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void refresh(Object arg0, LockModeType arg1) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void refresh(Object arg0, LockModeType arg1, Map<String, Object> arg2) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void detach(Object arg0) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public LockModeType getLockMode(Object arg0) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setProperty(String arg0, Object arg1) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public Map<String, Object> getProperties() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public <T> TypedQuery<T> createQuery(CriteriaQuery<T> arg0) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public <T> TypedQuery<T> createQuery(String arg0, Class<T> arg1) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public <T> TypedQuery<T> createNamedQuery(String arg0, Class<T> arg1) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public <T> T unwrap(Class<T> arg0) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public EntityManagerFactory getEntityManagerFactory() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public CriteriaBuilder getCriteriaBuilder() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public Metamodel getMetamodel() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+public class EntityManagerBean implements EntityManager,Serializable {
+
+	private EntityManager emDelegate;
+
+	@PostConstruct
+	public void createEntityManager() {
+	
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("geodojodbspatial");
+			emDelegate = emf.createEntityManager();
+		
+	}
+	
+	public void destroyEntityManager()
+	{
+		if (emDelegate == null) {
+			emDelegate.flush();
+			emDelegate.close();
+		}
+	}
+	
+	@Override
+	public void clear() {
+		
+		this.emDelegate.clear();
+		
+	}
+
+	@Override
+	public void close() {
+		
+		this.emDelegate.close();
+		
+	}
+
+	@Override
+	public boolean contains(Object arg0) {
+		
+		return this.emDelegate.contains(arg0);
+	}
+
+	@Override
+	public Query createNamedQuery(String arg0) {
+		
+		return this.emDelegate.createNamedQuery(arg0);
+	}
+
+	@Override
+	public Query createNativeQuery(String arg0) {
+		
+		return this.emDelegate.createNamedQuery(arg0);
+	}
+
+	@Override
+	public Query createNativeQuery(String arg0, Class arg1) {
+		
+		return this.emDelegate.createNativeQuery(arg0, arg1);
+	}
+
+	@Override
+	public Query createNativeQuery(String arg0, String arg1) {
+		
+		return this.emDelegate.createNativeQuery(arg0, arg1);
+	}
+
+	@Override
+	public Query createQuery(String arg0) {
+		
+		return this.emDelegate.createQuery(arg0);
+	}
+
+	@Override
+	public <T> T find(Class<T> arg0, Object arg1) {
+		
+		return this.emDelegate.find(arg0, arg1);
+	}
+
+	@Override
+	public void flush() {
+		this.emDelegate.flush();
+		
+	}
+
+	@Override
+	public Object getDelegate() {
+		
+		return this.emDelegate.getDelegate();
+	}
+
+	@Override
+	public FlushModeType getFlushMode() {
+		// TODO Auto-generated method stub
+		return this.emDelegate.getFlushMode();
+	}
+
+	@Override
+	public <T> T getReference(Class<T> arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		return this.emDelegate.getReference(arg0, arg1);
+	}
+
+	@Override
+	public EntityTransaction getTransaction() {
+		// TODO Auto-generated method stub
+		return this.emDelegate.getTransaction();
+	}
+
+	@Override
+	public boolean isOpen() {
+		// TODO Auto-generated method stub
+		return this.emDelegate.isOpen();
+	}
+
+	@Override
+	public void joinTransaction() {
+		this.emDelegate.joinTransaction();
+		
+	}
+
+	@Override
+	public void lock(Object arg0, LockModeType arg1) {
+		this.emDelegate.lock(arg0, arg1);
+		
+	}
+
+	@Override
+	public <T> T merge(T arg0) {
+		
+		return this.emDelegate.merge(arg0);
+	}
+
+	@Override
+	public void persist(Object arg0) {
+		this.emDelegate.persist(arg0);
+		
+	}
+
+	@Override
+	public void refresh(Object arg0) {
+		this.emDelegate.refresh(arg0);
+		
+	}
+
+	@Override
+	public void remove(Object arg0) {
+		this.emDelegate.remove(arg0);
+		
+	}
+
+	@Override
+	public void setFlushMode(FlushModeType arg0) {
+		this.emDelegate.setFlushMode(arg0);
+		
+	}
+
+	
+	
+	
+	
 }
