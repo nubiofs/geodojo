f5d5bba025e07fce7e3a14f254f9df9efb66c5d6
ranomail <ranomail@e6791326-9150-4ff1-db7a-2f0afe25d0be>
2010-10-17 01:34:21 +0000

    Refactorings

--- implementation/app/geodojo/src/main/java/org/latinoware/geodojo/beans/EntityManagerBean.java
+++ implementation/app/geodojo/src/main/java/org/latinoware/geodojo/beans/EntityManagerBean.java
@@ -1,6 +1,7 @@
 package org.latinoware.geodojo.beans;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.SessionScoped;
@@ -11,6 +12,10 @@ import javax.persistence.FlushModeType;
 import javax.persistence.LockModeType;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.metamodel.Metamodel;
 
 /**
  * Managed Bean(CDI) criado para disponibilizar o EntityManager em toda a aplicacao.
@@ -21,162 +26,246 @@ import javax.persistence.Query;
  */
 @SuppressWarnings("serial")
 @SessionScoped
-public class EntityManagerBean implements EntityManager,Serializable {
+public class EntityManagerBean implements EntityManager, Serializable {
 
-	private EntityManager emDelegate;
+    private EntityManager emDelegate;
 
-	@PostConstruct
-	public void createEntityManager() {
-	
-			EntityManagerFactory emf = Persistence.createEntityManagerFactory("geodojodbspatial");
-			emDelegate = emf.createEntityManager();
-		
-	}
-	
-	public void destroyEntityManager()
-	{
-		if (emDelegate == null) {
-			emDelegate.flush();
-			emDelegate.close();
-		}
-	}
-	
-	@Override
-	public void clear() {
-		
-		this.emDelegate.clear();
-		
-	}
+    @PostConstruct
+    public void createEntityManager() {
 
-	@Override
-	public void close() {
-		
-		this.emDelegate.close();
-		
-	}
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("geodojodbspatial");
+        emDelegate = emf.createEntityManager();
 
-	@Override
-	public boolean contains(Object arg0) {
-		
-		return this.emDelegate.contains(arg0);
-	}
+    }
 
-	@Override
-	public Query createNamedQuery(String arg0) {
-		
-		return this.emDelegate.createNamedQuery(arg0);
-	}
+    public void destroyEntityManager() {
+        if (emDelegate == null) {
+            emDelegate.flush();
+            emDelegate.close();
+        }
+    }
 
-	@Override
-	public Query createNativeQuery(String arg0) {
-		
-		return this.emDelegate.createNamedQuery(arg0);
-	}
+    @Override
+    public void clear() {
 
-	@Override
-	public Query createNativeQuery(String arg0, Class arg1) {
-		
-		return this.emDelegate.createNativeQuery(arg0, arg1);
-	}
+        this.emDelegate.clear();
 
-	@Override
-	public Query createNativeQuery(String arg0, String arg1) {
-		
-		return this.emDelegate.createNativeQuery(arg0, arg1);
-	}
+    }
 
-	@Override
-	public Query createQuery(String arg0) {
-		
-		return this.emDelegate.createQuery(arg0);
-	}
+    @Override
+    public void close() {
 
-	@Override
-	public <T> T find(Class<T> arg0, Object arg1) {
-		
-		return this.emDelegate.find(arg0, arg1);
-	}
+        this.emDelegate.close();
 
-	@Override
-	public void flush() {
-		this.emDelegate.flush();
-		
-	}
+    }
 
-	@Override
-	public Object getDelegate() {
-		
-		return this.emDelegate.getDelegate();
-	}
+    @Override
+    public boolean contains(Object arg0) {
 
-	@Override
-	public FlushModeType getFlushMode() {
-		// TODO Auto-generated method stub
-		return this.emDelegate.getFlushMode();
-	}
+        return this.emDelegate.contains(arg0);
+    }
 
-	@Override
-	public <T> T getReference(Class<T> arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		return this.emDelegate.getReference(arg0, arg1);
-	}
+    @Override
+    public Query createNamedQuery(String arg0) {
 
-	@Override
-	public EntityTransaction getTransaction() {
-		// TODO Auto-generated method stub
-		return this.emDelegate.getTransaction();
-	}
+        return this.emDelegate.createNamedQuery(arg0);
+    }
 
-	@Override
-	public boolean isOpen() {
-		// TODO Auto-generated method stub
-		return this.emDelegate.isOpen();
-	}
+    @Override
+    public Query createNativeQuery(String arg0) {
 
-	@Override
-	public void joinTransaction() {
-		this.emDelegate.joinTransaction();
-		
-	}
+        return this.emDelegate.createNamedQuery(arg0);
+    }
 
-	@Override
-	public void lock(Object arg0, LockModeType arg1) {
-		this.emDelegate.lock(arg0, arg1);
-		
-	}
+    @Override
+    public Query createNativeQuery(String arg0, Class arg1) {
 
-	@Override
-	public <T> T merge(T arg0) {
-		
-		return this.emDelegate.merge(arg0);
-	}
+        return this.emDelegate.createNativeQuery(arg0, arg1);
+    }
 
-	@Override
-	public void persist(Object arg0) {
-		this.emDelegate.persist(arg0);
-		
-	}
+    @Override
+    public Query createNativeQuery(String arg0, String arg1) {
 
-	@Override
-	public void refresh(Object arg0) {
-		this.emDelegate.refresh(arg0);
-		
-	}
+        return this.emDelegate.createNativeQuery(arg0, arg1);
+    }
 
-	@Override
-	public void remove(Object arg0) {
-		this.emDelegate.remove(arg0);
-		
-	}
+    @Override
+    public Query createQuery(String arg0) {
 
-	@Override
-	public void setFlushMode(FlushModeType arg0) {
-		this.emDelegate.setFlushMode(arg0);
-		
-	}
+        return this.emDelegate.createQuery(arg0);
+    }
 
-	
-	
-	
-	
+    @Override
+    public <T> T find(Class<T> arg0, Object arg1) {
+
+        return this.emDelegate.find(arg0, arg1);
+    }
+
+    @Override
+    public void flush() {
+        this.emDelegate.flush();
+
+    }
+
+    @Override
+    public Object getDelegate() {
+
+        return this.emDelegate.getDelegate();
+    }
+
+    @Override
+    public FlushModeType getFlushMode() {
+        // TODO Auto-generated method stub
+        return this.emDelegate.getFlushMode();
+    }
+
+    @Override
+    public <T> T getReference(Class<T> arg0, Object arg1) {
+        // TODO Auto-generated method stub
+        return this.emDelegate.getReference(arg0, arg1);
+    }
+
+    @Override
+    public EntityTransaction getTransaction() {
+        // TODO Auto-generated method stub
+        return this.emDelegate.getTransaction();
+    }
+
+    @Override
+    public boolean isOpen() {
+        // TODO Auto-generated method stub
+        return this.emDelegate.isOpen();
+    }
+
+    @Override
+    public void joinTransaction() {
+        this.emDelegate.joinTransaction();
+
+    }
+
+    @Override
+    public void lock(Object arg0, LockModeType arg1) {
+        this.emDelegate.lock(arg0, arg1);
+
+    }
+
+    @Override
+    public <T> T merge(T arg0) {
+
+        return this.emDelegate.merge(arg0);
+    }
+
+    @Override
+    public void persist(Object arg0) {
+        this.emDelegate.persist(arg0);
+
+    }
+
+    @Override
+    public void refresh(Object arg0) {
+        this.emDelegate.refresh(arg0);
+
+    }
+
+    @Override
+    public void remove(Object arg0) {
+        this.emDelegate.remove(arg0);
+
+    }
+
+    @Override
+    public void setFlushMode(FlushModeType arg0) {
+        this.emDelegate.setFlushMode(arg0);
+
+    }
+
+    @Override
+    public <T> T find(Class<T> arg0, Object arg1, Map<String, Object> arg2) {
+        return this.emDelegate.find(arg0, arg1, arg2);
+    }
+
+    @Override
+    public <T> T find(Class<T> arg0, Object arg1, LockModeType arg2) {
+        return this.emDelegate.find(arg0, arg1, arg2);
+    }
+
+    @Override
+    public <T> T find(Class<T> arg0, Object arg1, LockModeType arg2, Map<String, Object> arg3) {
+        return this.emDelegate.find(arg0, arg1, arg2, arg3);
+    }
+
+    @Override
+    public void lock(Object arg0, LockModeType arg1, Map<String, Object> arg2) {
+        this.emDelegate.lock(arg0, arg1, arg2);
+    }
+
+    @Override
+    public void refresh(Object arg0, Map<String, Object> arg1) {
+        this.emDelegate.refresh(arg0, arg1);
+    }
+
+    @Override
+    public void refresh(Object arg0, LockModeType arg1) {
+        this.emDelegate.refresh(arg0, arg1);
+    }
+
+    @Override
+    public void refresh(Object arg0, LockModeType arg1, Map<String, Object> arg2) {
+        this.emDelegate.refresh(arg0, arg1, arg2);
+    }
+
+    @Override
+    public void detach(Object arg0) {
+        this.emDelegate.detach(arg0);
+    }
+
+    @Override
+    public LockModeType getLockMode(Object arg0) {
+        return this.emDelegate.getLockMode(arg0);
+    }
+
+    @Override
+    public void setProperty(String arg0, Object arg1) {
+        this.emDelegate.setProperty(arg0, arg1);
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        return this.emDelegate.getProperties();
+    }
+
+    @Override
+    public <T> TypedQuery<T> createQuery(CriteriaQuery<T> arg0) {
+        return this.emDelegate.createQuery(arg0);
+    }
+
+    @Override
+    public <T> TypedQuery<T> createQuery(String arg0, Class<T> arg1) {
+        return this.emDelegate.createQuery(arg0, arg1);
+    }
+
+    @Override
+    public <T> TypedQuery<T> createNamedQuery(String arg0, Class<T> arg1) {
+        return this.emDelegate.createNamedQuery(arg0, arg1);
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> arg0) {
+        return this.emDelegate.unwrap(arg0);
+    }
+
+    @Override
+    public EntityManagerFactory getEntityManagerFactory() {
+        return this.emDelegate.getEntityManagerFactory();
+    }
+
+    @Override
+    public CriteriaBuilder getCriteriaBuilder() {
+        return this.emDelegate.getCriteriaBuilder();
+    }
+
+    @Override
+    public Metamodel getMetamodel() {
+        return this.emDelegate.getMetamodel();
+    }
 }
